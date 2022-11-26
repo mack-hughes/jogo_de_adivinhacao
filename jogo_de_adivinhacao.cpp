da014342d724d282d9900c6824a6fee620b54f33
mack hughes <mackhughes251@gmail.com>
2022-11-26 14:32:59 -0300

    First commit

---
+++ jogo_de_adivinhacao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "****************************************************" <<  endl;
+    cout << "******    Bem vindo ao jogo de adivinhação    ******" <<  endl;
+    cout << "****************************************************" <<  endl;
+}
